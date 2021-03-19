046e9ca97b3dda59bdc2e17edb33cf305774e550
marcelo1811 <miyachimarcelo@gmail.com>
2021-03-19 19:37:00 -0300

    feat(NewTransactionModal): add style to buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -27,6 +29,18 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
 
         <input placeholder='Título'/>
         <input placeholder='Valor' type='number'/>
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="entrada"/>
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="saída"/>
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
         <input placeholder='Categoria'/>
         <button type="submit">
           Cadastrar
